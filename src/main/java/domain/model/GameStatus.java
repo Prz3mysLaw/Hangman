634105a0a404f732b785e4bb2e9997b2ff7d8ad7
bboyKamel <nacoon@tlen.pl>
2019-02-13 18:39:27 +0100

    Create GameStatus Class

---
+++ src/main/java/domain/model/GameStatus.java
@@ -0,0 +1,45 @@
+package domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class GameStatus {
+    private String phrase;
+    private Character[] phraseState;
+    private Integer maxAttempts;
+    private Integer successAttemps;
+    private Integer failedAttempts;
+    private List<Character> history;
+
+    public GameStatus(String phrase, Integer maxAttempts) {
+        this.phrase = phrase;
+        this.phraseState = phraseState;
+        this.maxAttempts = maxAttempts;
+        this.successAttemps = 0;
+        this.failedAttempts = 0;
+        this.history = new ArrayList<Character>();
+    }
+
+    public boolean isGameFinished(){
+        if (failedAttempts >= maxAttempts){
+            return true;
+        }
+        for (Character character : phraseState) {
+            if (character == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+}
+
+
